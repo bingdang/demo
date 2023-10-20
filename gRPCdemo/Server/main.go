bd4c389f1db14d27a012363f74abbc55ecf47b67
fei <431965560@qq.com>
2023-10-20 19:32:44 +0800

    grpc

--- gRPCdemo/Server/main.go
+++ gRPCdemo/Server/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"gRPCdemo/pb"
+	"google.golang.org/grpc"
+	"net"
+)
+
 /*
+1.
 go get -u google.golang.org/grpc
 https://github.com/protocolbuffers/protobuf/releases/tag/v3.20.3
 
@@ -12,6 +21,32 @@ protoc              protoc-gen-go       protoc-gen-go-grpc
 
 */
 
+// 定义服务
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// 定义方法
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Answer: "Hello " + in.Name}, nil
+}
+
 func main() {
+	// 向gRPC注册服务
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+		return
+	}
+
+	s := grpc.NewServer()                  //创建gRPC服务器
+	pb.RegisterGreeterServer(s, &server{}) //在gRPC服务端注册服务
+
+	//启动服务
+	err = s.Serve(listen)
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+		return
+	}
 
 }
